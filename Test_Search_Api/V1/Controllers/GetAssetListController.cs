62e2888c96e3c8d0c7e281b8ea5f1e3b4deea84d
lbhMichael <michael0ndukwe@gmail.com>
2022-08-11 15:21:54 +0100

    added terraform_es setup

---
+++ Test_Search_Api/V1/Controllers/GetAssetListController.cs
@@ -0,0 +1,77 @@
+using Amazon.Lambda.Core;
+using Hackney.Core.Logging;
+using Test_Search_Api.V1.Boundary.Requests;
+using Test_Search_Api.V1.Boundary.Responses;
+using Test_Search_Api.V1.Boundary.Responses.Metadata;
+using Test_Search_Api.V1.UseCase.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Test_Search_Api.Controllers
+{
+    [ApiVersion("1")]
+    [Produces("application/json")]
+    [Route("api/v1/search/assets")]
+    [ApiController]
+    public class GetAssetListController : BaseController
+    {
+        private readonly IGetAssetListUseCase _getAssetListUseCase;
+        private readonly IGetAssetListSetsUseCase _getAssetListSetsUseCase;
+
+        public GetAssetListController(IGetAssetListUseCase getAssetListUseCase, IGetAssetListSetsUseCase getAssetListSetsUseCase)
+        {
+            _getAssetListUseCase = getAssetListUseCase;
+            _getAssetListSetsUseCase = getAssetListSetsUseCase;
+        }
+
+        [ProducesResponseType(typeof(APIResponse<GetAssetListResponse>), 200)]
+        [ProducesResponseType(typeof(APIResponse<NotFoundException>), 404)]
+        [ProducesResponseType(typeof(APIResponse<BadRequestException>), 400)]
+        [HttpGet, MapToApiVersion("1")]
+        [LogCall(LogLevel.Information)]
+        public async Task<IActionResult> GetAssetList([FromQuery] GetAssetListRequest request)
+        {
+            try
+            {
+                var assetsSearchResult = await _getAssetListUseCase.ExecuteAsync(request).ConfigureAwait(false);
+                var apiResponse = new APIResponse<GetAssetListResponse>(assetsSearchResult);
+                apiResponse.Total = assetsSearchResult.Total();
+
+                return new OkObjectResult(apiResponse);
+            }
+            catch (Exception e)
+            {
+                LambdaLogger.Log(e.Message + e.StackTrace);
+                return new BadRequestObjectResult(e.Message);
+            }
+        }
+
+        [ProducesResponseType(typeof(APIResponse<GetAllAssetListResponse>), 200)]
+        [ProducesResponseType(typeof(APIResponse<NotFoundException>), 404)]
+        [ProducesResponseType(typeof(APIResponse<BadRequestException>), 400)]
+        [Route("all")]
+        [HttpGet, MapToApiVersion("1")]
+        [LogCall(LogLevel.Information)]
+        public async Task<IActionResult> GetAllAssetList([FromQuery] GetAllAssetListRequest request)
+        {
+            try
+            {
+                var assetsSearchResult = await _getAssetListSetsUseCase.ExecuteAsync(request).ConfigureAwait(false);
+                var apiResponse = new APIAllResponse<GetAllAssetListResponse>(assetsSearchResult)
+                {
+                    Total = assetsSearchResult.Total(),
+                    LastHitId = assetsSearchResult.LastHitId()
+                };
+
+                return new OkObjectResult(apiResponse);
+            }
+            catch (Exception e)
+            {
+                LambdaLogger.Log(e.Message + e.StackTrace);
+                return new BadRequestObjectResult(e.Message);
+            }
+        }
+    }
+}
--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -1,12 +1,3 @@
-# INSTRUCTIONS: 
-# 1) ENSURE YOU POPULATE THE LOCALS 
-# 2) ENSURE YOU REPLACE ALL INPUT PARAMETERS, THAT CURRENTLY STATE 'ENTER VALUE', WITH VALID VALUES 
-# 3) YOUR CODE WOULD NOT COMPILE IF STEP NUMBER 2 IS NOT PERFORMED!
-# 4) ENSURE YOU CREATE A BUCKET FOR YOUR STATE FILE AND YOU ADD THE NAME BELOW - MAINTAINING THE STATE OF THE INFRASTRUCTURE YOU CREATE IS ESSENTIAL - FOR APIS, THE BUCKETS ALREADY EXIST
-# 5) THE VALUES OF THE COMMON COMPONENTS THAT YOU WILL NEED ARE PROVIDED IN THE COMMENTS
-# 6) IF ADDITIONAL RESOURCES ARE REQUIRED BY YOUR API, ADD THEM TO THIS FILE
-# 7) ENSURE THIS FILE IS PLACED WITHIN A 'terraform' FOLDER LOCATED AT THE ROOT PROJECT DIRECTORY
-
 provider "aws" {
   region  = "eu-west-2"
   version = "~> 2.0"
@@ -14,44 +5,76 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 locals {
-  application_name = your application name # The name to use for your application
-   parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
-}
-
-
-data "aws_iam_role" "ec2_container_service_role" {
-  name = "ecsServiceRole"
-}
-
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+  parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
 }
 
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-development-apis"
+    bucket  = "terraform-state-housing-development"
     encrypt = true
     region  = "eu-west-2"
-    key     = services/YOUR API NAME/state #e.g. "services/transactions-api/state"
+    key     = "services/test-search-api/state"
   }
 }
 
-module "development" {
-  # Delete as appropriate:
-  source                      = "github.com/LBHackney-IT/Test_Search_Api//terraform/development/main.tf"
-  cluster_name                = "development-apis"
-  ecr_name                    = ecr repository name # Replace with your repository name - pattern: "hackney/YOUR APP NAME"
-  environment_name            = "development"
-  application_name            = local.application_name 
-  security_group_name         = back end security group name # Replace with your security group name, WITHOUT SPECIFYING environment. Usually the SG has the name of your API
-  vpc_name                    = "vpc-development-apis"
-  host_port                   = port # Replace with the port to use for your api / app
-  port                        = port # Replace with the port to use for your api / app
-  desired_number_of_ec2_nodes = number of nodes # Variable will only be used if EC2 is required. Do not remove it. 
-  lb_prefix                   = "nlb-development-apis"
-  ecs_execution_role          = data.aws_iam_role.ecs_task_execution_role.arn
-  lb_iam_role_arn             = data.aws_iam_role.ec2_container_service_role.arn
-  task_definition_environment_variables = {
-    ASPNETCORE_ENVIRONMENT = "development"
+/*    ELASTICSEARCH SETUP    */
+
+data "aws_vpc" "development_vpc" {
+  tags = {
+    Name = "test_search_api_dev"
   }
+}
+
+data "aws_subnet_ids" "development" {
+  vpc_id = data.aws_vpc.development_vpc.id
+  filter {
+    name   = "tag:Type"
+    values = ["private"]
+  }
+}
+
+module "elasticsearch_db_development" {
+  source           = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/database/elasticsearch"
+  vpc_id           = data.aws_vpc.development_vpc.id
+  environment_name = "development"
+  port             = 443
+  domain_name      = "test_search_api_es"
+  subnet_ids       = [tolist(data.aws_subnet_ids.development.ids)[0]]
+  project_name     = "test_search_api"
+  es_version       = "7.8"
+  encrypt_at_rest  = "true"
+  instance_type    = "t3.small.elasticsearch"
+  instance_count   = "2"
+  ebs_enabled      = "true"
+  ebs_volume_size  = "10"
+  region           = data.aws_region.current.name
+  account_id       = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_ssm_parameter" "search_elasticsearch_domain" {
+  name  = "/test-search-api/development/elasticsearch-domain"
+  type  = "String"
+  value = "https://${module.elasticsearch_db_development.es_endpoint_url}"
+}
+
+module "housing_search_api_cloudwatch_dashboard" {
+  source                  = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudwatch/dashboards/api-dashboard"
+  environment_name        = var.environment_name
+  api_name                = "test_search_api"
+  include_sns_widget      = false
+  include_dynamodb_widget = false
+  no_sns_widget_dashboard = false
+}
+
+data "aws_ssm_parameter" "cloudwatch_topic_arn" {
+  name = "/housing-tl/${var.environment_name}/cloudwatch-alarms-topic-arn"
+}
+
+module "api-alarm" {
+  source           = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudwatch/api-alarm"
+  environment_name = var.environment_name
+  api_name         = "test_search_api"
+  alarm_period     = "300"
+  error_threshold  = "1"
+  sns_topic_arn    = data.aws_ssm_parameter.cloudwatch_topic_arn.value
 }
