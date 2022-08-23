bba48274920f87ba9a5823334450ba962bb63a39
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2022-08-23 16:52:31 +0100

    amending response model

---
+++ Test_Search_Api/V1/Gateways/QueryableAsset.cs
@@ -0,0 +1,26 @@
+﻿namespace Test_Search_Api.V1.Gateways
+{
+    public class QueryableAsset
+    {
+        [Text(Name = "id")]
+        public string Id { get; set; }
+
+        [Text(Name = "title")]
+        public string Title { get; set; }
+
+        [Text(Name = "firstName")]
+        public string FirstName { get; set; }
+
+        [Text(Name = "lastName")]
+        public string LastName { get; set; }
+
+        [Text(Name = "dob")]
+        public string DOB { get; set; }
+
+        [Text(Name = "assetType")]
+        public string AssetType { get; set; }
+
+        [Text(Name = "numberOfBedrooms")]
+        public int NumberOfBedrooms { get; set; }
+    }
+}
