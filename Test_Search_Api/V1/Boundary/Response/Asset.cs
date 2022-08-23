ac68ea05d20ed32280f18a782e4afdfef6720629
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2022-08-23 09:40:45 +0100

    response object

---
+++ Test_Search_Api/V1/Boundary/Response/Asset.cs
@@ -0,0 +1,12 @@
+﻿namespace Test_Search_Api.V1.Boundary.Response
+{
+    public class Asset
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DOB { get; set; }
+        public string AssetType { get; set; }
+    }
+}
