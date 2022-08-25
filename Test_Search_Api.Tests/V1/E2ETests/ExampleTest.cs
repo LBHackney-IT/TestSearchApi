fe59e0e332fd6de9eb02d15f09dba049c6539549
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2022-08-25 16:31:04 +0100

    file removal

--- Test_Search_Api.Tests/V1/E2ETests/ExampleTest.cs
+++
@@ -1,8 +0,0 @@
-namespace Test_Search_Api.Tests.V1.E2ETests
-{
-    //For guidance on writing integration tests see the wiki page https://github.com/LBHackney-IT/lbh-test-search-api/wiki/Integration-Tests
-    public class ExampleTest : IntegrationTests<Startup>
-    {
-
-    }
-}
